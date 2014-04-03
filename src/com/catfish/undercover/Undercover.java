5ace4f71ba3a22538e247bf5a1617dbb2ccb245b
Wenzhao <wzhfang@marvell.com>
2014-04-03 11:04:07 +0800

    add libs and clean the main entry
    Signed-off-by: Wenzhao <wzhfang@marvell.com>

--- src/com/catfish/undercover/Undercover.java
+++ src/com/catfish/undercover/Undercover.java
@@ -1,15 +1,12 @@
 package com.catfish.undercover;
 
-import java.lang.reflect.Method;
-
 import android.content.Context;
 import android.util.Log;
-
-import com.catfish.undercover.HookedMethod.HookedCallback;
 
 public class Undercover {
     public final static String TAG = "catfish";
 
     public void onInject(Context application) {
         Log.e(TAG, "hook starts");
+    }
 }
