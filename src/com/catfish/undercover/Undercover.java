51e3fdf85af58650a704c405348a5e7b3d9e10a9
Wenzhao <wzhfang@marvell.com>
2014-04-03 11:04:07 +0800

    add libs and clean the main entry
    Signed-off-by: Wenzhao <wzhfang@marvell.com>

--- src/com/catfish/undercover/Undercover.java
+++ src/com/catfish/undercover/Undercover.java
@@ -12,26 +12,4 @@ public class Undercover {
 
     public void onInject(Context application) {
         Log.e(TAG, "hook starts");
-        Method[] hookMethod = null;
-        try {
-            Class actyclass = Class.forName("com.tencent.mm.ui.contact.profile.ContactInfoUI");
-            hookMethod = actyclass.getDeclaredMethods();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        HookedCallback h = new HookedCallback() {
-
-            @Override
-            public Object invoke(HookedMethod method, Object receiver, Object[] args) {
-                for (Object obj : args) {
-                    Log.d(TAG, "hooked: " + method.getName() + ", " + obj.getClass());
-                }
-                new Exception("got " + method.getName()).printStackTrace();
-                return method.invoke(receiver, args);
-            }
-        };
-        for (Method m : hookMethod) {
-            HookManager.setMethodHooked(m, h);;
-        }
-    }
 }
